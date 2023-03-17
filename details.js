fca6cd656355f69bb7f11e2b5eae7dc47b6998c7
Sebastian Local <imal_66@hotmail.com>
2023-03-17 19:26:19 -0300

    practico js 3 -correccion de valor de id en el detail -  funciona la busqueda, las categorias y el details

--- details.js
+++ details.js
@@ -7,14 +7,14 @@ const params = new URLSearchParams(queryString)   // instanciamos una url de una
 const id = params.get("id") // obtenemos al id que esta en el home.js y es: ${eventos._id}
                             // para el boton de info
 console.log(id)
-const eventoParaMostrarDetails = eventos.find(evento => eventos._id == id)
+const eventoParaMostrarDetails = eventos.find(evento => eventos._id === id)
 
 const div = document.querySelector("#fichas")
 div.innerHTML = ` <div class="card mb-3">
                         <div class="card-body">
-                            <img class="card-img-top" src="${eventos[id].image}" alt="${eventos[id].name}">
-                            <h5 class="card-title">${eventos[id].name}</h5>
-                            <p class="card-text">${eventos[id].description}</p>
+                            <img class="card-img-top" src="${eventos[id-1].image}" alt="${eventos[id-1].name}">
+                            <h5 class="card-title">${eventos[id-1].name}</h5>
+                            <p class="card-text">${eventos[id-1].description}</p>
                             <a href="./index.html" class="boton-ficha">Volver</a>
                         </div>
                     </div> `
